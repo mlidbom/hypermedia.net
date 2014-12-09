aa414ef874179419c2b8f4a16fbd8ada9ed3a7e2
mlidbo <magnus.lidbom@gmail.com>
2014-12-09 02:16:52 +0100

    Refactor dependency on web api out of the RouteValues class and into the web api specific LambdaBasedUrlHelper

--- Hypermedia.WebApi/WebApi/LamdaBasedUrlHelper.cs
+++ Hypermedia.WebApi/WebApi/LamdaBasedUrlHelper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Web.Http;
 using System.Web.Http.Routing;
 
 namespace Composable.Hypermedia.WebApi
@@ -14,8 +16,28 @@ namespace Composable.Hypermedia.WebApi
 
         private static string Url(this UrlHelper @this, LambdaExpression controllerCall)
         {
-            var routeValues = new RouteValues(controllerCall);
-            return routeValues.CreateLink(@this);            
+            return @this.CreateLink(new RouteValues(controllerCall));            
+        }
+
+        private static string CreateLink(this UrlHelper @this, RouteValues routeValues)
+        {
+            var values = routeValues.Values.ToDictionary(pair => pair.Key, pair => pair.Value);
+            if (routeValues.ControllerType.IsSubclassOf(typeof(ApiController)))
+            {
+                values.Remove("controller");
+                values.Remove("action");
+            }
+            return @this.Link(routeValues.RouteName(), values);
+        }
+
+        private static string RouteName(this RouteValues @this)
+        {
+            var routeAttribute = @this.ControllerCall.Method.GetCustomAttributes(typeof(RouteAttribute), false).OfType<RouteAttribute>().ToList();
+            if (routeAttribute.Any())
+            {
+                return routeAttribute.Single().Name;
+            }
+            return null;
         }
     }
 }
--- Hypermedia.WebApi/WebApi/RouteValues.cs
+++ Hypermedia.WebApi/WebApi/RouteValues.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Web.Http;
-using System.Web.Http.Routing;
 using Composable.Hypermedia.Linq.Expressions;
 
 namespace Composable.Hypermedia.WebApi
@@ -11,7 +9,7 @@ namespace Composable.Hypermedia.WebApi
     public class RouteValues
     {
         private readonly LambdaExpression _lambda;
-        private readonly MethodCallExpression _controllerCall;
+        public readonly MethodCallExpression ControllerCall;
         private readonly Dictionary<string, object> _values = new Dictionary<string, object>();
 
         public static RouteValues Create<TController, TResponse>(Expression<Func<TController, TResponse>> controllerCall)
@@ -23,9 +21,8 @@ namespace Composable.Hypermedia.WebApi
         {
             _lambda = controllerMethodCall;
 
-            _controllerCall = ExtractControllerCall();
+            ControllerCall = ExtractControllerCall();
 
-            RouteName = GetRouteName();
             _values["controller"] = Controller = ExtractControllerName();
             _values["action"] = Action;
 
@@ -37,28 +34,18 @@ namespace Composable.Hypermedia.WebApi
 
         private ArgumentSpecification[] ExtractArguments()
         {
-            return _controllerCall.Arguments.Select(
+            return ControllerCall.Arguments.Select(
                 (argument, index) => new ArgumentSpecification
                           {
                               Index = index,
                               Argument = argument,
-                              ParameterName = _controllerCall.Method.GetParameters()[index].Name
+                              ParameterName = ControllerCall.Method.GetParameters()[index].Name
                           }).ToArray();
-        }
-
-        private string GetRouteName()
-        {
-            var routeAttribute = _controllerCall.Method.GetCustomAttributes(typeof(RouteAttribute), false).OfType<RouteAttribute>().ToList();
-            if(routeAttribute.Any())
-            {
-                return routeAttribute.Single().Name;
-            }
-            return null;
-        }
+        }        
 
         private string ExtractControllerName()
         {
-            ControllerType = _controllerCall.Method.DeclaringType;
+            ControllerType = ControllerCall.Method.DeclaringType;
             var controller = ControllerType.Name;
             return controller.ToLower().EndsWith("controller") ? controller.Substring(0, controller.Length - 10) : controller;
         }
@@ -77,27 +64,15 @@ namespace Composable.Hypermedia.WebApi
         public Type ControllerType { get; private set; }
         public string Controller { get; private set; }
 
-        public string Action { get { return _controllerCall.Method.Name; } }
+        public string Action { get { return ControllerCall.Method.Name; } }
 
         public IReadOnlyDictionary<string, object> Values { get { return _values; } }
-        public string RouteName { get; set; }
 
         internal class ArgumentSpecification
         {
             public int Index { get; set; }
             public Expression Argument { get; set; }
             public string ParameterName { get; set; }
-        }
-
-        public string CreateLink(UrlHelper @this)
-        {
-            var values = Values.ToDictionary(pair => pair.Key, pair => pair.Value);
-            if(ControllerType.IsSubclassOf(typeof(ApiController)))
-            {
-                values.Remove("controller");
-                values.Remove("action");
-            }
-            return @this.Link(RouteName, values);
         }
     }
 }
