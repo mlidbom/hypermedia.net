5949e498cba0dbf5d64a7bb2789d3dcc75628de8
mlidbo <magnus.lidbom@gmail.com>
2014-12-14 15:41:27 +0100

    Make sure that package references are copy local.

--- Hypermedia.WebApi.Tests/When_generating_links/Link_is_correct_for_method_with_string_arguments.cs
+++ Hypermedia.WebApi.Tests/When_generating_links/Link_is_correct_for_method_with_string_arguments.cs
@@ -3,10 +3,9 @@ using System.Web.Http;
 using NUnit.Framework;
 
 namespace Hypermedia.WebApi.Tests.When_generating_links
-{
-   
+{   
     [TestFixture]
-    public class Link_is_correct_when_generating_to_method_with_string_arguments : UrlGenerationTest
+    public class Link_is_correct_for_method_with_string_arguments : UrlGenerationTest
     {
         private const string OneArgumentRouteTemplate = "AB59FD02-8874-4A0D-A29C-0988BEEE8992/echo/{text}";
         private const string TwoArgumentsRouteTemplate = "0CA44C8F-D5B1-41BC-9D0A-964CDB9443E4/twoargs/{arg1}/{arg2}";
